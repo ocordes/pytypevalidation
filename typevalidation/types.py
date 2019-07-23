050e98ae59296c191c01b0ae926428a1b5993251
Oliver Cordes <ocordes@astro.uni-bonn.de>
2019-07-23 08:21:56 +0200

    - add Matrix type - fill out setup.py

--- typevalidation/types.py
+++ typevalidation/types.py
@@ -3,7 +3,7 @@
 typevalidation/types.py
 
 written by: Oliver Cordes 2019-07-04
-changed by: Oliver Cordes 2019-07-21
+changed by: Oliver Cordes 2019-07-22
 
 
 """
@@ -56,3 +56,31 @@ def Vector(x):
             raise TypeError('string array needs 3 entries for a vector')
     else:
         raise TypeError('x is not of a suitable type for a vector')
+
+
+# check if x is a 3x3 matrix
+def Matrix(x):
+    if isinstance(x, (list, tuple)):
+        # try to convert the lists into a numpy array
+        try:
+            x = np.array(x, dtype=np.float64)
+        except:
+            x = np.array(0)
+        if x.shape == (3, 3):
+            return x
+        else:
+            raise TypeError('x is not a 3x3 matrix with lists/tuples')
+    elif isinstance(x, np.ndarray):
+        if x.shape == (3, 3):
+            return x.copy()
+        else:
+            raise TypeError('x is not a 3x3 numpy array')
+    elif isinstance(x, str):
+        s = x.split(',')
+        if len(s) == 9:
+            return np.array([float(i) for i in s],
+                            dtype=np.float64).reshape(3, 3)
+        else:
+            raise TypeError('string array needs 9 entries for a 3x3 matrix')
+    else:
+        raise TypeError('x is not of a suitable type for a 3x3 matrix')
